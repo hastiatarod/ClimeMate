343393a3a1e3bf0f9d38dc06dfdf03723e10b78a
Hasti <45739032+hastiatarod@users.noreply.github.com>
2024-10-23 21:26:27 +0330

    rounding temperature

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temp");
-  temperatureElement.innerHTML = response.data.temperature.current;
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
