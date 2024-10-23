f10dccc0bff93fb5ca60354ee6457ba4eb76f0fe
Hasti <45739032+hastiatarod@users.noreply.github.com>
2024-10-23 21:19:36 +0330

    added api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = response.data.temperature.current;
+}
+
+function searchCity(city) {
+  let apiKey = "tb9bafb403447563b62afb93o4d8c0fd";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
